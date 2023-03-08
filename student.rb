b14878b964905b201f557b3aa5b6aa1521401122
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-08 17:40:20 +0300

    Feat: Update student class

--- student.rb
+++ student.rb
@@ -1,6 +1,9 @@
 require './person'
+require './classroom'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -8,5 +11,10 @@ class Student < Person
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def add_class(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
