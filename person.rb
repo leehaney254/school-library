4e4c0c908683201277e21e5c1c019ef7d0e3fbcd
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-09 20:49:23 +0300

    Feat: Update Person class

--- person.rb
+++ person.rb
@@ -4,9 +4,9 @@ require './rental'
 class Person < Nameable
   attr_reader :id
 
-  attr_accessor :age, :name, rental
+  attr_accessor :age, :name, :rental
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, parent_permission, name = 'Unknown')
     super()
     @id = Random.rand(1..1000)
     @name = name
