68edb0a80d28cbda71544ce80862f4b568a95d2a
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-10 10:32:23 +0300

    Feat: Add bug

--- main.rb
+++ main.rb
@@ -82,15 +82,15 @@ def invalid_option
 end
 
 def parameter_option(parameter)
-  options = {
-    '1' => method(:list_books),
-    '2' => method(:list_people),
-    '3' => method(:option_create_person),
-    '4' => method(:option_create_book),
-    '5' => method(:option_create_rental),
-    '6' => method(:option_list_rentals),
-    '7' => method(:exit)
-  }
+  # options = {
+  #   '1' => method(:list_books),
+  #   '2' => method(:list_people),
+  #   '3' => method(:option_create_person),
+  #   '4' => method(:option_create_book),
+  #   '5' => method(:option_create_rental),
+  #   '6' => method(:option_list_rentals),
+  #   '7' => method(:exit)
+  # }
 
   option_method = options[parameter] || method(:invalid_option)
   option_method.call
